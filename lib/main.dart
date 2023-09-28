51761d776506ce243fb9f95b7eafcd127401331c
selinguz <selin-guzel@hotmail.com>
2023-09-28 14:25:44 +0300

    named routes oluşturuldu, static keyword üne giriş yapıldı

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute:WelcomeScreen.id, //home property yerine kullanıldı
+      routes: {
+        ChatScreen.id:(context) => ChatScreen(),
+        LoginScreen.id:(context) => LoginScreen(),
+        RegistrationScreen.id:(context) => RegistrationScreen(),
+        WelcomeScreen.id:(context) => WelcomeScreen(),
+      },
+    );
+  }
+}
